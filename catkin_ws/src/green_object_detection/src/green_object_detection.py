7f4c2820209d897f26ec0f1cdfb212b4a832e573
jorgevh113 <A00827318>
2023-05-03 10:42:55 -0600

    "Detect_green_object"

--- catkin_ws/src/green_object_detection/src/green_object_detection.py
+++ catkin_ws/src/green_object_detection/src/green_object_detection.py
@@ -1,34 +1,53 @@
-#!/usr/bin/env python3
-
-import rospy
-from sensor_msgs.msg import Image
-from cv_bridge import CvBridge
 import cv2
+import numpy as np
+import rospy
+from sensor_msgs.msg import Imagefrom cv_bridge import CvBridge
+
+rospy.init_node('detect_green_node')
+bridge = CvBridge()
+
+green_object_pub = rospy.Publisher('green_object_coordinates', Point, qeue_size=10)
+
+# Procesamiento de la imagen
+def process_image(img):
+    # CONVERSIÒN DE IMAGEN DE ros A OBJETO CV
+    cv_image = bridge.imgmsg_to_cv2(img,"bgr8")
+    # CONVERSIÒN DE CV_IMAGE A ESCALA DE GRISES
+    gray_image = cv2.cvtColor(cv_image, cv2.COLOR_BGR2GRAY)
+    # CREACIÒN DE UMBRAL PARA UBICAR LOS OBJETOS VERDES, DESPUÈS SE ENCUENTRAN LOS CONTORNOS
+    _,thresh_image = cv2.threshold(gray:image, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+    # IDENTIFICACIÒN DE CONTORNOS DE LOS OBJETOS SOBRE LA IMAGEN UMBRALIZADA
+    contours,_ = cv2.findContours(thresh_image, cv2.RETR.LIST, cv2.CHAIN_APPROX_SIMPLE)
+
+    # BUSCAMOS EL CONTORNO MÀXIMO
+    max_area = 0
+    max_contour = None
+    for contour in contours:
+        area = cv2.contourArea(contour)
+        if area > max_area:
+            max_area = area
+            max_contour = contour
+    
+    # AHORA CALCULAMOS LAS COORDENADAS X,Y DEL CENTRO DEL OBJETO VERDE
+    if max_contour is not None:
+        M = cv2.moments(max_contour)
+        cx = int(M['m10'] / M['m00'])
+        cy = int(M['m01'] / M['m00']))
+        # PUBLICAMOS LAS COORDENADAS DEL OBJETO VERDE
+        green_object_pub.publish(Point(cx,cy,0))
+
+    # MOSTRAMOS LA IMAGEN PROCESADA
+    cv2.imshow('Processed Image', cv_image)
+    cv2.waitKey(1)
+
+# Devoluciòn de la llamada para la imagen
+def image_callback(img):
+    process_image(img)
 
 
-class GreenObjectDetector:
-
-    def __init__(self):
-        self.bridge = CvBridge()
-        self.image_sub = rospy.Subscriber("/camera/image_raw", Image, self.callback)
-
-    def callback(self, data):
-        cv_image = self.bridge.imgmsg_to_cv2(data, "bgr8")
-        hsv_image = cv2.cvtColor(cv_image, cv2.COLOR_BGR2HSV)
-        lower_green = (35, 50, 50)
-        upper_green = (80, 255, 255)
-        mask = cv2.inRange(hsv_image, lower_green, upper_green)
-        contours, _ = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-        for cnt in contours:
-            area = cv2.contourArea(cnt)
-            if area > 500:
-                x, y, w, h = cv2.boundingRect(cnt)
-                cv2.rectangle(cv_image, (x, y), (x + w, y + h), (0, 255, 0), 2)
-        cv2.imshow("Green Object Detector", cv_image)
-        cv2.waitKey(1)
+# CREAMOS EL OBJETO DE SUSCRIPCIÒN A LA IMAGEN
+image_sub = rospy.Subscriber('/camera/image_raw', Image, image_callback) 
 
 
-if __name__ == '__main__':
-    rospy.init_node('green_object_detection_node')
-    detector = GreenObjectDetector()
-    rospy.spin()
+rospy.spin()
+cv2.destroyAllWindows()  
