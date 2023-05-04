3cf61c446c7bd64565592d4effb212b15670f63f
jorgevh113 <A00827318>
2023-05-03 18:52:56 -0600

    "Added library to rosnode"

--- catkin_ws/src/green_object_detection/src/green_object_detection.py
+++ catkin_ws/src/green_object_detection/src/green_object_detection.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3
-
-
 import rospy
+import ctypes
 from sensor_msgs.msg import Image
 from geometry_msgs.msg import Point
 from cv_bridge import CvBridge
 import cv2
 
+coordLibrary = ctypes.CDLL("/home/javh/Documents/octavo/arturo/actInterfases/catkin_ws/src/green_object_detection/src/systemObject/libmylibrary.so")
 
 class GreenObjectDetector:
 
@@ -22,12 +22,18 @@ class GreenObjectDetector:
         upper_green = (80, 255, 200)
         mask = cv2.inRange(hsv_image, lower_green, upper_green)
         contours, _ = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+        coordY = ctypes.c_float(0)
+        coordX = ctypes.c_float(0)
+        coordZ = ctypes.c_float(0)
         for cnt in contours:
             area = cv2.contourArea(cnt)
             if area > 500:
                 x, y, w, h = cv2.boundingRect(cnt)
                 cv2.rectangle(cv_image, (x, y), (x + w, y + h), (0, 255, 0), 2)
-                self.pub_coord.publish(Point((x+w)/2,(y+h)/2,0))
+                coordY.value = (y+h)/2
+                coordX.value = (x+w)/2
+                coordLibrary.calculateCoords(ctypes.pointer(coordX), ctypes.pointer(coordY), ctypes.pointer(coordZ))
+                self.pub_coord.publish(Point(coordX.value, coordY.value, coordZ.value))
         cv2.imshow("Green Object Detector", cv_image)
         cv2.waitKey(1)
 
